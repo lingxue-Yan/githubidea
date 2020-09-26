1aa377c4b3f2f3a3ab8026ab9d79ef1591db6ddd
Ywl <2048360499@qq.com>
2020-09-26 22:20:57 +0800

    在IDEA中的git练习

---
+++ src/main/java/com/git/Test.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/*
+ * @Author Ywl
+ * @Date 2020/9/26 22:03
+ *
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("今天是一个好日子");
+    }
+}
